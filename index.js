236117033b8a62995ed11c5380f53eb6e22f5006
Samson Ameh <amehsam69@gmail.com>
2023-11-24 04:22:42 +0100

    Post loop added

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const posts = [
     name: "Vincent van Gogh",
     username: "vincey1853",
     location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
+    avatar: "assets/images/avatar-1.png",
+    post: "assets/images/post-image-1.png",
     comment: "just took a few mushrooms lol",
     likes: 21,
   },
@@ -12,8 +12,8 @@ const posts = [
     name: "Gustave Courbet",
     username: "gus1819",
     location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
+    avatar: "assets/images/avatar-2.png",
+    post: "assets/images/post-image-2.png",
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
   },
@@ -21,8 +21,8 @@ const posts = [
     name: "Joseph Ducreux",
     username: "jd1735",
     location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
+    avatar: "assets/images/avatar-3.png",
+    post: "assets/images/post-image-3.png",
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
@@ -30,13 +30,49 @@ const posts = [
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("hello");
+  createPost(posts);
 });
 
+// create post
 
-function createPost(posts){
+function createPost(postArray) {
+  // loop through the array of objects
+  for (let i = 0; i < postArray.length; i++) {
+    let postEl = document.querySelector("#post-section");
 
-for(let i = 0; )
+    postEl.innerHTML += `
+            <div class="post-container container">
+                <div class="user-info">
+                    <img class="avatar-width" src="${postArray[i].avatar}"/>
+                    <div>
+                        <h2 class="fs-l fw-700">${postArray[i].name}</h2>
+                        <p class="fs-m fw-400">${postArray[i].location}</p>
+                    </div>
+                </div>
+    
+                <img class="post-img" src="${postArray[i].post}"/>
+    
+                <div class="icons-container">
+                    <div>
+                        <i class="likeBtn icons fa fa-heart-o" aria-hidden="true"></i>
+                        <i class="icons fa fa-comment-o" aria-hidden="true"></i>
+                        <i class="icons fa fa-share-alt-square" aria-hidden="true"></i>
+                    </div>
+    
+                    <h3 class="likes fs-l fw-700"><span class="likeCount">${postArray[i].likes}</span> likes</h3>
+                    <p class="username-caption fs-m fw-400">
+                        <span class="username fw-700">${postArray[i].username}</span> ${postArray[i].comment}
+                    </p>
+                </div>
+            </div>
+        `;
+  }
 
+  let likeBtn = document.querySelector(".likeBtn");
+  let likeEl = document.querySelector(".likeCount");
 
+  let likes = 0;
+  likeBtn.addEventListener("click", () => {
+    console.log("hello");
+  });
 }
