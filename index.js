c700d8b3a4435252b7be8cec8312065bc4c9b33d
Samson Ameh <amehsam69@gmail.com>
2023-11-24 12:15:45 +0100

    The kike button function added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const posts = [
     post: "assets/images/post-image-1.png",
     comment: "just took a few mushrooms lol",
     likes: 21,
+    isLiked: false,
   },
   {
     name: "Gustave Courbet",
@@ -16,6 +17,7 @@ const posts = [
     post: "assets/images/post-image-2.png",
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
+    isLiked: false,
   },
   {
     name: "Joseph Ducreux",
@@ -26,6 +28,7 @@ const posts = [
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
+    isLiked: false,
   },
 ];
 
@@ -33,15 +36,14 @@ document.addEventListener("DOMContentLoaded", () => {
   createPost(posts);
 });
 
-// create post
-
 function createPost(postArray) {
   // loop through the array of objects
   for (let i = 0; i < postArray.length; i++) {
     let postEl = document.querySelector("#post-section");
+    let newPostContainer = document.createElement("div");
+    newPostContainer.classList.add("post-container", "container");
 
-    postEl.innerHTML += `
-            <div class="post-container container">
+    newPostContainer.innerHTML = `
                 <div class="user-info">
                     <img class="avatar-width" src="${postArray[i].avatar}"/>
                     <div>
@@ -49,35 +51,65 @@ function createPost(postArray) {
                         <p class="fs-m fw-400">${postArray[i].location}</p>
                     </div>
                 </div>
-    
+
                 <img class="post-img" src="${postArray[i].post}"/>
-    
+
                 <div class="icons-container">
                     <div>
-                        <i class="likeBtn icons fa fa-heart-o" aria-hidden="true"></i>
+
+                    <i class="likeBtn icons fa ${
+                      postArray[i].isLiked ? "fa-heart" : "fa-heart-o"
+                    }" aria-hidden="true"></i>
+
                         <i class="icons fa fa-comment-o" aria-hidden="true"></i>
                         <i class="icons fa fa-share-alt-square" aria-hidden="true"></i>
                     </div>
-    
-                    <h3 class="likes fs-l fw-700"><span class="likeCount">${postArray[i].likes}</span> likes</h3>
+
+                    <h3 class="likes fs-l fw-700"><span class="likeCount">${
+                      postArray[i].likes
+                    }</span> likes</h3>
                     <p class="username-caption fs-m fw-400">
-                        <span class="username fw-700">${postArray[i].username}</span> ${postArray[i].comment}
+                        <span class="username fw-700">${
+                          postArray[i].username
+                        }</span> ${postArray[i].comment}
                     </p>
                 </div>
-            </div>
+            
         `;
+    postEl.appendChild(newPostContainer);
 
-    // Select the like button and like count for the current post
-    let likeBtn = document.querySelector(".likeBtn");
-    let likeEl = document.querySelector(".likeCount");
+    // Select the likeBtn and likeCount within the current post container
 
-    let countFromArray = postArray[i];
-    let currentCount = countFromArray[1];
+    let likeBtn = newPostContainer.querySelector(".likeBtn");
+    let likeCount = newPostContainer.querySelector(".likeCount");
 
+    // add event to the button
     likeBtn.addEventListener("click", () => {
-      // Increase the like count for the specific post when the like button is clicked
-      postArray[i].likes++;
-      likeEl.textContent = ` ${postArray[i].likes}`;
+      toggleLikeState(postArray[i]);
+      updateLikeButton(likeBtn, likeCount, postArray[i]);
     });
   }
+}
+
+function toggleLikeState(post) {
+  post.isLiked = !post.isLiked;
+
+  if (post.isLiked) {
+    // Increment like count if post is liked
+    post.likes += 1;
+  } else {
+    // Decrement like count if post is unliked
+    post.likes -= 1;
+  }
+}
+
+function updateLikeButton(likeBtn, likeCount, post) {
+  if (post.isLiked) {
+    likeBtn.classList.remove("fa-heart-o");
+    likeBtn.classList.add("fa-heart");
+  } else {
+    likeBtn.classList.remove("fa-heart");
+    likeBtn.classList.add("fa-heart-o");
+  }
+  likeCount.innerText = post.likes;
 }
